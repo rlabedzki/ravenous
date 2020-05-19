a20103c1e1ce680e884a7d482c49fd042e60fbd5
rlabedzki <rlabedzki@gmail.com>
2020-05-19 11:34:51 +0200

    part 2

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,21 +15,24 @@ class SearchBar extends React.Component{
             return <li key={sortByOptionValue}>{sortByOption}</li>
         });
     }
+
     render(){
-        <div className="SearchBar">
-            <div className="SearchBar-sort-options">
-                <ul>
-                    {this.renderSortByOptions()}
-                </ul>
-            </div>
-            <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
-            </div>
-            <div className="SearchBar-submit">
-                <a>Let's Go</a>
-            </div>
+        return(
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
         </div>
+        )
     }
 }
 
